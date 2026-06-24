f2ed8f0c4687acfd1a7324d3a51bbef60fe6e3d6
l140221 <l140221@users.noreply.github.com>
2026-06-24 17:57:40 +0800

    init: Spring Boot project

---
+++ src/main/java/com/example/Springboot240304Application.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot240304Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot240304Application.class, args);
+    }
+}
---
+++ src/test/java/com/example/Springboot240304ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot240304ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
